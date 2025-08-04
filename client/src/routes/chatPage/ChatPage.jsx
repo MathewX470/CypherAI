5bf93d00b3596aa1db25b99d1586f8fed4480404
mathew~ <mathewpunnen8281@gmail.com>
2025-08-04 23:59:05 +0530

    updated chatpage

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,44 @@
-import './chatPage.css'
+import { useRef } from "react";
+import "./chatPage.css";
+import { useEffect } from "react";
 
 const ChatPage = () => {
+  
   return (
-    <div className='chatPage'>ChatPage</div>
-  )
-}
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message ai lore</div>
+          <div className="message user">
+            Test Message user Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Impedit odit, unde alias animi pariatur officia repudiandae
+            modi dolores perspiciatis amet temporibus quibusdam itaque autem.
+            Nulla similique alias expedita cumque reprehenderit?
+          </div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+          <div className="message">Test Message ai</div>
+          <div className="message user">Test Message user</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChatPage
+export default ChatPage;
